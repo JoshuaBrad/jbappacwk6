8636ab1168d5cd43cd19c678b8d9e44ac819b767
JoshuaBrad <85682477+JoshuaBrad@users.noreply.github.com>
2025-06-16 20:19:38 +0200

    .

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
 function getContacts(){
-alert("bobthebuilder")
+    fetch(rootPath + "controller/get-contacts/")
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        // displayOutput(data);
+        console.log(data);
+    })
+}
+
+function displayOutput(){
+
 }
